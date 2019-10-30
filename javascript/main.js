e3de286afea0a6f19c51b05f19c72a5f0ca81ca7
patpatfc <batuhan.bayraktar@ggl.hisarschool.k12.tr>
2019-10-30 15:54:05 +0300

    update

--- javascript/main.js
+++ javascript/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     setTimeout(function(){
         $('body').addClass('loaded');
         $('h1').css('color','#222222');
-    }, 3000/3000);//şimidlik bölü 3000 yaptım çalışmalarımı hızlandırmak amacıyla
+    }, 3000);//şimidlik bölü 3000 yaptım çalışmalarımı hızlandırmak amacıyla
 
 	// when menu button is clicked
 	document.getElementById('open-menu-button').addEventListener('click', function(){
