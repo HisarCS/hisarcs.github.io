76bc977f1e08f37cfd594ff5bb9c11a872b0f17b
Terobero <kaanbozkurt2001@gmail.com>
2020-12-20 17:41:08 +0300

    Language support

---
+++ assets/js/language.js
@@ -0,0 +1,12 @@
+let button = document.getElementById("switch-lang");
+
+if (document.cookie) {
+    if (document.cookie.includes("lang=tr")) { $('[lang="en"]').hide(); document.documentElement.lang = "tr-TR"; }
+    else { $('[lang="tr"]').hide(); document.documentElement.lang = "en-US"; }
+} else { $('[lang="tr"]').hide(); document.documentElement.lang = "en-US"; }
+
+$('#switch-lang').click(function() {
+    if (button.innerText == "EN") { button.innerText = "TR"; document.documentElement.lang = "en-US"; document.cookie = "lang=en"; }
+    else { button.innerText = "EN"; document.documentElement.lang = "tr-TR"; document.cookie = "lang=tr"; };
+    $('[lang="tr"]').toggle(); $('[lang="en"]').toggle();
+});
